b550e1da71f3241f0c5880bba72e1538ce20670f
JICaddick <james_caddick16@hotmail.com>
2023-04-15 22:03:58 -0500

    edits

--- components/Hero.js
+++ components/Hero.js
@@ -2,21 +2,21 @@ import Link from 'next/link'
 
 export default function Hero() {
   return (
-    <div className="my-48 mx-auto max-w-7xl px-4 sm:mt-24 md:mt-72 text-center">
+    <div className="px-4 mx-auto my-48 text-center max-w-7xl sm:mt-24 md:mt-72">
       <h1 className="font-extrabold text-gray-900">
         <p className="text-xl sm:text-3xl md:text-4xl">
-          Shopify + Next.js + Tailwind CSS:
+          Learn Some English, BIATCHHHHH:
         </p>
-        <p className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-4xl sm:text-6xl md:text-7xl">
-          Modern eCommerce
+        <p className="text-4xl text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 sm:text-6xl md:text-7xl">
+          Quit sounding like a dumdum infront of colleagues 
         </p>
       </h1>
-      <h2 className="mt-3 max-w-md mx-auto text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-x-3xl">
-        Build the eCommerce Revolution
+      <h2 className="max-w-md mx-auto mt-3 text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-x-3xl">
+        Send me yo moniessss and learn some English, cuz 
       </h2>
-      <div className="mt-5 max-w-md mx-auto flex justify-center items-center md:mt-8">
+      <div className="flex items-center justify-center max-w-md mx-auto mt-5 md:mt-8">
         <Link href="#">
-          <a className="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium py-3 border-transparent rounded-md text-white bg-gray-900 hover:bg-gray-800">
+          <a className="inline-flex items-center justify-center h-12 px-6 py-3 mr-6 font-medium text-white bg-gray-900 border-transparent rounded-md hover:bg-gray-800">
             Enroll Now
           </a>
         </Link>
