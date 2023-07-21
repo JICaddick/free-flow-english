1e85ce67b7fb9cc33dcdfa307128ed6ed027dcfc
JICaddick <james_caddick16@hotmail.com>
2023-07-20 21:00:37 -0500

    whitespace for trigger

--- components/RecommendedList.js
+++ components/RecommendedList.js
@@ -14,7 +14,7 @@ const RecommendedList = ({ products, current }) => {
                         ))
                     }
                 </div> 
-            </div>
+            </div> 
         </div>
     ) 
 }
