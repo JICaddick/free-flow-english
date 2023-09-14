be12a6e5d0efc2be7ba219ab0488c1c079cf5f8e
JICaddick <james_caddick16@hotmail.com>
2023-09-13 20:12:05 -0500

    unstyled content added-first drafts. Edits pending.

--- pages/about.js
+++ pages/about.js
@@ -18,6 +18,14 @@ const AboutUs = () => {
           <h3 className="text-sm text-center text-gray-600">Business English Coach</h3>
           <p className="pb-6 mt-2">
             Andrew is an experienced English language teacher based in Mexico with a proven record in English language teaching and academic management over the last 8 years.
+
+            Prior to teaching, Andrew graduated from Nottingham Trent University with a Bachelor's degree in Business Management and Accounting & Finance, which included a 1-year work placement in the finance department of a prominent pharmaceutical company. During this role Andrew developed his knowledge of Accounting and Finance procedures within a fast-paced international environment. His position also gave him a wide-range of experience working with clients and colleagues both internally and externally. Despite being offered a full-time role at the company following his graduation, Andrew chose to move to Mexico in order to fulfill his desire of living abroad in order to learn about a new culture and a new language.
+
+            Andrew currently works as a freelance Business English teacher and examiner, having spent 6 years previously working in a variety of schools in Mexico, first as a teacher, and then in academic management roles. Andrew's experience has allowed him to work with a range of students ranging across different cultures and varying levels of English. His management experience has also given him an insight into the challenges faced within businesses when it comes to recruitment, reducing staff turnover, developing effective teams and handling client relations. He has also led a number of projects such as the Model United Nations and the inter school debate team, which have allowed his students to develop their English skills whilst also increasing awareness of global issues. 
+
+            Alongside his current job, Andrew also works as a speaking and writing examiner for Cambridge English and IELTS exams. This experience has allowed him to further understand what is required from his students in order to develop their professional English skills and achieve their language learning goals.
+
+            Andrew is currently looking for new opportunities, in and out of Mexico, to further his professional development whether it be in the education industry or within the world of business and finance. He has a strong desire to remain working in an international environment, which will allow him to learn new languages in a professional capacity. Outside of work, Andrew loves the outdoors, enjoys all sports and is a keen runner.  
           </p>
         </div>
       </div>
@@ -36,9 +44,15 @@ const AboutUs = () => {
         />
         <div className="text-center">
           <h2 className="text-2xl font-semibold text-center">James Ian Caddick</h2>
-          <h3 className="text-sm text-center text-gray-600">Business English Expert</h3>
+          <h3 className="text-sm text-center text-gray-600"> Business English Coach | Full stack Developer | Co-founder | Director at JIC Investments Ltd. | Family man | Philantropist | </h3>
           <p className="pb-6 mt-2">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida urna quis dolor lacinia, vel malesuada dolor faucibus.
+            James is a professional language coach with over 3 years of experience and success in coaching business professionals and young adults. With a wealth of experience in several fields his interests and experience are broad in scope. Having cofounded multiple large cultural and entertainment venues across Latin America he understands the pressures and challenges of managing business internationally in a second language. As well as co-managing one of these venues, James is currently operating a few Short Term Let properties in the south and south west of England, with a view to building a larger property portfolio through developments over the coming year.
+
+            He graduated from The University of Derby with a Bachelor of Science in Psychology before working as an electrical technician and going on to a rewarding employment with Jaguar Land Rover where he earnt an NVQ level 2 in business improvement techniques.
+
+            James is also a full stack developer with a passion for creating and building dynamic web applications. Skilled in JavaScript, Typescript, React, Redux, Next.js, Ruby, RoR, Node, PostgreSQL, MariaDB, Jest, Rspec, and Shopify, he has built and maintained several websites and web applications for his own businesses and for clients; in fact, he coded this very website! Having spent time working for a California based CRM start-up he understands the impossible nature of juggling a fulltime tech career with his other business interests and family life so, for now, coding is more of a sidehustle and a hobby than a fulltime career.
+            
+            As well as his business endeavours he's been supporting his wife in the running and operations of Colombian NGO, Concretemos. Initially supporting victims of violence and displaced citizens, the NGO has gone on to broaden its horizons and now also focuses on social and environmental projects in the districts of Antioquia and El Meta.
           </p>
         </div>
       </div>
--- pages/termsandconditions.js
+++ pages/termsandconditions.js
@@ -9,24 +9,63 @@ const TermsAndConditions = () => {
         </h1>
 
         <div className="prose">
+
           <h2>1. Introduction</h2>
-          <p>
-            Welcome to our website. These terms and conditions apply to your
-            use of this website. By using this website, you accept these terms
-            and conditions in full. If you disagree with these terms and
-            conditions or any part of these terms and conditions, you must not
-            use this website.
-          </p>
 
-          <h2>2. Intellectual Property Rights</h2>
-          <p>
-            Unless otherwise stated, we or our licensors own the intellectual
-            property rights in the website and material on the website. Subject
-            to the license below, all these intellectual property rights are
-            reserved.
-          </p>
+          <p>1.1 These terms and conditions ("Terms") govern the provision of business English coaching services ("Services") by [Your Business Name], hereinafter referred to as "we," "us," or "our," to South Korean business executives, hereinafter referred to as "clients" or "you."</p>
 
-          {/* Add more sections as needed */}
+          <h2>2. Service Description</h2>
+
+          <p>2.1 Our Services aim to provide tailored, one-on-one business-focused English language tuition to enhance your English language skills for professional purposes.</p>
+
+          <h2>3. Payment Terms</h2>
+
+          <p>3.1 Payment for Services is due in advance and must be made in accordance with the payment method specified by us.</p>
+
+          <p>3.2 All fees are non-refundable unless otherwise specified in writing.</p>
+
+          <h2>4. Cancellation and Rescheduling</h2>
+
+          <p>4.1 Cancellations: If you wish to cancel a scheduled coaching session, you must provide us with at least [number] hours' notice. Failure to do so may result in a forfeiture of that session.</p>
+
+          <p>4.2 Rescheduling: You may request to reschedule a coaching session with at least [number] hours' notice, subject to our availability. Rescheduled sessions must take place within 7 days of the original scheduled session date.</p>
+
+          <h2>5. Client Commitment</h2>
+
+          <p>5.1 To maximize the benefits of our Services, you agree to actively participate, complete assigned coursework, and adhere to any recommendations provided by our coaches.</p>
+
+          <h2>6. Confidentiality</h2>
+
+          <p>6.1 We will treat all client information as confidential and will not disclose it to third parties without your consent, except as required by law.</p>
+
+          <h2>7. Termination</h2>
+
+          <p>7.1 We reserve the right to terminate our coaching relationship with you if you engage in any behavior that is disruptive or violates these Terms.</p>
+
+          <h2>8. Dispute Resolution</h2>
+
+          <p>8.1 In the event of a dispute arising from these Terms, we will attempt to resolve it through negotiation. If a resolution cannot be reached, the dispute may be escalated to mediation or legal action as necessary.</p>
+
+          <h2>9. Governing Law</h2>
+
+          <p>9.1 These Terms are governed by and construed in accordance with the laws of [Your Jurisdiction], and any disputes will be subject to the exclusive jurisdiction of the courts of [Your Jurisdiction].</p>
+
+          <h2>10. Changes to Terms</h2>
+
+          <p>10.1 We reserve the right to modify these Terms at any time. Any changes will be communicated to you in writing.</p>
+
+          <h2>11. Intellectual Property Rights</h2>
+
+          <p>11.1 All session plans and related content created by us remain our intellectual property. Clients are granted a limited, non-transferable license to use these materials solely for the purpose of the coaching sessions. Any reproduction, distribution, or use of these materials beyond the scope of the coaching sessions without our express written consent is prohibited.</p>
+
+          <h2>12. Recording of Sessions</h2>
+
+          <p>12.1 We record coaching sessions to ensure quality and provide valuable feedback to our coaches. Clients have the right to decline session recording by submitting a written request. In such cases, no recordings will be made.</p>
+
+          <p>12.2 For clients who consent to session recording, we may request to use these recordings for promotional purposes, such as testimonials or demonstrations. However, such usage will only occur with the client's explicit written consent.</p>
+
+          <h2>By availing of our Services, you acknowledge that you have read, understood, and agree to be bound by these Terms and Conditions.</h2>
+
         </div>
       </div>
     </div>
