a87196d49fb2588db122408f9a7654a99d1899e0
JICaddick <james_caddick16@hotmail.com>
2023-09-20 15:37:12 -0500

    about the company section added

--- pages/index.js
+++ pages/index.js
@@ -38,6 +38,36 @@ export default function Home({ products }) {
         <meta property="og:site_name" content="Modern eCommerce Course" />
       </Head>
       <Hero />
+    {/* Start of "about the company" section */}
+    <div className="py-10 bg-white">
+      <div className="container mx-auto">
+        {/* About the company content */}
+        <h2 className="mb-4 text-3xl font-semibold">About Our Company</h2>
+        <p className="text-lg text-gray-700">
+        We're Andy and James and We offer private Business English Communication Classes to help you develop your language skills and feel more confident when communicating in a Business environment.
+
+Free Get-To-Know-You Class
+This will help you identify your goals so that we can create the pathway to achieve them.
+
+Private Sessions
+One-to-one sessions ensure that the sessions are specific to your circumstances & needs, so that you achieve your goals more quickly.
+
+Flexible Scheduling to meet your needs (mornings, evenings and weekends)
+Organise your sessions around your work and family life.
+
+In-session Feedback
+Develop language immediately to help you speak more concisely and confidently.
+
+Personalised Sessions
+Topics will be specific to your needs and knowledge areas
+
+Sign up today and make an impact on the rest of your career!
+
+Learn more about the coaches here*.
+        </p>
+      </div>
+    </div>
+    {/* End of "about the company" section */}
       <ProductList products={products} />
     </div>
   ) 
