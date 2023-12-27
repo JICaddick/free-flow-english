5bfa20bd39be6f9bdc8910aa5a533f6b986e332e
JICaddick <james_caddick16@hotmail.com>
2023-12-26 23:06:36 -0500

    removed arrayceil and addeded eager loading to try and improve load time on hero image

--- components/Hero.js
+++ components/Hero.js
@@ -1,24 +1,9 @@
 import Image from "next/image";
-import useDimensions from 'react-cool-dimensions';
-import arrayCeil from '../lib/arrayCeil';
-import React, { useState } from 'react'
 
 const Hero = () => {
 
-  const [heroImage, setHeroImage] = useState()
-  const imageSizes = [600, 1280, 1920]  
-  
-  const { observe } = useDimensions({
-    onResize: ({ observe, unobserve, width, height }) => {
-      setHeroImage(`hero-${arrayCeil(imageSizes, width, height)}.jpg`)
-
-      unobserve(); // To stop observing the current target element
-      observe(); // To re-start observing the current target element
-    },
-  });
-
   return (
-    <div ref={observe} className="relative h-[32rem]">
+    <div className="relative h-[32rem]">
       <Image
       style={{filter: "blur(1px)", transform: "scale(1.1)", opacity: "0.95", position: "absolute", top: "0", left: "0", width: "100%", height: "75%",}}
         src="/sky_line.jpg"
@@ -26,6 +11,7 @@ const Hero = () => {
         layout="fill"
         objectFit="cover"
         quality={100}
+        loading="eager"
       />
       <div className="absolute inset-0 flex flex-col items-center justify-center font-extrabold text-center text-white text-stroke-4 text-stroke-black">
         <p className="text-7xl sm:text-6xl md:text-7xl lg:text-8xl lg:pt-10 font-lora" style={{ textShadow: "25px 25px 25px black" }}>
